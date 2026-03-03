8e589f4bd685ab33bfc014e7fdac45502e7a3ce5
krishnakamith <krishnakamith@gmail.com>
2026-03-03 10:39:43 +0530

    icon and name changed

--- reelscout/reelscout/settings.py
+++ reelscout/reelscout/settings.py
@@ -22,7 +22,8 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#z0s3z+9gduzn1mhgk5p7p6_^=g-h@6u8k3zk=(l3tlp1w@(qm'
+# settings.py
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-fallback-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
